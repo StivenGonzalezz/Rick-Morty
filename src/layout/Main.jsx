02222f7082327befed256a131063b4ddd4a85f7e
Stiven <olajhace2002@gmail.com>
2023-11-06 23:46:18 -0500

    Agregado de buscador

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -14,6 +14,7 @@ function Main() {
   const [aleatorioExtraterrestres,setAleatorioExtraterrestres]=useState("")
   const [aleatorioVivos,setAleatorioVivos]=useState("")
   const [aleatorioNoVivos,setAleatorioNoVivos]=useState("")
+  const [response,setResponse]=useState(true)
 
   useEffect(()=>{
     apiCharacters()
@@ -50,6 +51,16 @@ function Main() {
     return await axios.get(`https://rickandmortyapi.com/api/character/?page=${aleatorioNoVivos}&status=Dead`).then((respuesta)=>{
       setPersonajes(respuesta.data.results)})
     }
+
+  const apiBuscador = async(e) =>{
+    return await axios.get(`https://rickandmortyapi.com/api/character/?name=${e}`).then((respuesta)=>{
+      setPersonajes(respuesta.data.results)
+      setResponse(true)
+    }).catch((error)=>{
+      console.log(error)
+      setResponse(false)
+    })
+     }
 
   const random = () =>{
     setAleatorioAside(String(parseInt(Math.random() * (41- 1) + 1)))
@@ -95,13 +106,18 @@ function Main() {
     apiAside()
   }
 
+  const filter = (e) =>{
+    console.log(e)
+    apiBuscador(e)
+  }
+
   let characters5 = personajes.slice(rango,rango+6)
   let charactersAside = personajesAside.slice(rango,rango+6)
  
 console.log(rango)
   return (
     <>
-    <nav id="navegationBar">
+    <nav className="flex justify-between mr-48 ml-5">
         <ul>
           <li onClick={()=>random()} className='inline-block mt-[2px] mr-[5px] font-bold cursor-pointer hover:text-green-500'>Aleatorio</li>
           <li onClick={()=>randomHumanos()} className='inline-block mt-[2px] mr-[5px] font-bold cursor-pointer hover:text-green-500'>Humanoides</li>
@@ -109,18 +125,21 @@ console.log(rango)
           <li onClick={()=>randomVivos()} className='inline-block mt-[2px] mr-[5px] font-bold cursor-pointer hover:text-green-500'>Personajes vivos</li>
           <li onClick={()=>randomNoVivos()} className='inline-block mt-[2px] mr-[5px] font-bold cursor-pointer hover:text-green-500'>Personajes muertos</li>
         </ul>
+        <input onChange={(e)=>filter(e.target.value)} placeholder='Buscar...' type="search" className='w-30 h-8 rounded-lg border-[1px] border-solid border-black pl-3'/>
       </nav>
     <div className="flex w-full h-[900px] px-[5px]">
-        <section className="border-[1px] border-solid border-black flex justify-evenly flex-wrap ">
+        <section className="border-[1px] border-solid border-black flex justify-evenly flex-wrap w-[100%]">
         {
+          response ? 
           characters5.map((character)=>{
             return(
             <Article personaje={character}/>
             )
           })
+          : <div>Personaje no encontrado</div>
         }
         </section>
-        <Aside className="mr-2" characters={charactersAside}/> 
+        <Aside characters={charactersAside}/> 
     </div>
     </>
   )
